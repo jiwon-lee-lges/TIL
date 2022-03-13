1b19bd048a1f1810bd9efb83c8eed24010f13ba2
Jiwon Lee <53148677+Jiwon-lee-me@users.noreply.github.com>
2022-03-13 17:22:00 +0900

    22.01.13 TIL

---
+++ [MySQL] SET @local_variable.sql
@@ -0,0 +1,10 @@
+SET @HOUR = -1;
+SELECT (@HOUR := @HOUR + 1) AS HOUR,
+    (SELECT COUNT(HOUR(DATETIME))  -- TO_NUMBER(TO_CHAR(DATETIME, 'HH24'))
+    FROM ANIMAL_OUTS
+    WHERE HOUR(DATETIME) = @HOUR) AS COUNT
+FROM ANIMAL_OUTS
+WHERE @HOUR < 23;
+
+### SET @local_variable command로 HOUR를 -1로 정의하고, 1씩 더해서 HOUR에 할당함 (0~23까지 만들어짐)
+### SubQuery를 사용해 주어진 데이터의 DATETIME HOUR과 새로 정의한 @HOUR가 같을 때 COUNT를 해줌
